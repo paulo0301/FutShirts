90a3bd47ff0c4ac96a0a2a11fbfa9c47a26ed7c0
Paulo Daniel <pdanielcdo11@gmail.com>
2021-01-25 17:16:24 -0300

    responsividade header

--- prototipos/js/login.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("input#entrar").onclick = function abrir(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.querySelector('.efeito').style.visibility = "visible";
-}
-document.querySelector("input#fechar").onclick = function fechar(){
-    document.querySelector('.efeito').style.visibility = "hidden";
-}

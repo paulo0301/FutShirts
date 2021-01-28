4e4db3a157da97fb84a21f7a5cbef4a6f409662b
Paulo Daniel <pdanielcdo11@gmail.com>
2021-01-28 16:01:00 -0300

    responsividade index 40%

--- prototipos/js/config_glider.js
+++ prototipos/js/config_glider.js
@@ -1,12 +1,30 @@
 window.addEventListener('load', function(){
     new Glider(document.querySelector('#glider01'), {
-        slidesToShow: 12,
-        slidesToScroll: 6,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         draggable: true,
         scrollLock: true,
+        arrows: false,
         arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        }
+            prev: '#glider-prev01',
+            next: '#glider-next01'
+        },
+        responsive: [
+            {
+                breakpoint: 675,
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 4,
+                    itemWidth: 150,
+                }
+            },{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 10,
+                    slidesToScroll: 5,
+                    itemWidth: 150,
+                }
+            }
+        ]
     });
-    })
+})

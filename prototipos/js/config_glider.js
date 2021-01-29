7d882cdc9fc1177a209ff46dc646084b622052ea
Paulo Daniel <pdanielcdo11@gmail.com>
2021-01-29 16:14:16 -0300

    responsividade index 95%

--- prototipos/js/config_glider.js
+++ prototipos/js/config_glider.js
@@ -27,4 +27,56 @@ window.addEventListener('load', function(){
             }
         ]
     });
-})
+    
+    
+});
+$(document).ready(function(){
+    $("#owl-carousel01").owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        lazyLoad: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            900: {
+                items: 3,
+            },
+            1300: {
+                items: 4,
+            }
+        }
+    })
+    /////////////////////////////////////
+    $("#owl-carousel02").owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+        lazyLoad: true,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            900: {
+                items: 3,
+            },
+            1300: {
+                items: 4,
+            }
+        }
+    })
+});
--- prototipos/js/main.js
+++ prototipos/js/main.js
@@ -18,3 +18,25 @@ btMenu.addEventListener('click', () => {
 fecharMenu.addEventListener('click', () => {
     menuLado.classList.remove("mostrar-menu");
 })
+
+$(document).ready(function(){
+    /////////////////////////////////////
+    $("section.vantagens ul").owlCarousel({
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        lazyLoad: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            700: {
+                items: 5,
+                loop: false,
+            }
+        }
+    })
+});

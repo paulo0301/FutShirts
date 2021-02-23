feed17b1cafdaf256e9a785c7f049a3d30b04c43
PedroP-code <pedropaulolucas@hotmail.com>
2021-02-23 00:39:27 -0300

    Criação do banco de dados

--- aplicacao asp/FutShirt/Context/UsuarioContext.cs
+++ aplicacao asp/FutShirt/Context/UsuarioContext.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using FutShirt.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
-using System.Data.Entity;
-using FutShirt.Models;
 
 namespace FutShirt.Context
 {
     public class UsuarioContext : DbContext
     {
-        public UsuarioContext() : base("Asp_Net_MVC_FS") { }
+        public UsuarioContext() : base("FutShirt") {
+            //Database.SetInitializer<UsuarioContext>(
+            //new DropCreateDatabaseIfModelChanges<UsuarioContext>());
+        }
 
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Cartao> Cartaos { get; set; }

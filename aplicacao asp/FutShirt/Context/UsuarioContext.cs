14e0cc1f4e8540124d259df3659bb7e78137c5fc
Paulo Daniel <pdanielcdo11@gmail.com>
2021-02-22 23:03:57 -0300

    add classs

---
+++ aplicacao asp/FutShirt/Context/UsuarioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using FutShirt.Models;
+
+namespace FutShirt.Context
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext() : base("Asp_Net_MVC_FS") { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cartao> Cartaos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+    }
+}
---
+++ aplicacao asp/FutShirt/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutShirt.Models
+{
+    public class Usuario
+    {
+        //Cadastro dados pessoais
+        public string nome, email, cpf, telefone, senha;
+        public DateTime nascimento;
+    }
+    public class Cartao
+    {
+        //Cadastro cartão
+        public long Id { get; set; }
+        public string nome_titular, cpf_titular, numero, csc;
+        public DateTime vencimento;
+    }
+    public class Endereco
+    {
+        //Cadastro endereço
+        public long Id { get; set; }
+        public string cep, estado, cidade, bairro, logradouro, numero, complemento;
+    }
+}

8bbf4f4d478b8ff36cb9ed80504667d4a0557e01
PedroP-code <pedropaulolucas@hotmail.com>
2021-02-25 00:33:26 -0300

    changes testes

--- aplicacao asp/FutShirt/Models/Usuario.cs
+++ aplicacao asp/FutShirt/Models/Usuario.cs
@@ -8,20 +8,33 @@ namespace FutShirt.Models
     public class Usuario
     {
         //Cadastro dados pessoais
-        public string nome, email, cpf, telefone, senha;
-        public DateTime nascimento;
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int cpf { get; set; }
+        public string telefone { get; set; }
+        public string senha { get; set; }
+        public DateTime nascimento { get; set; }
     }
     public class Cartao
     {
         //Cadastro cartão
-        public long Id { get; set; }
-        public string nome_titular, cpf_titular, numero, csc;
-        public DateTime vencimento;
+        public long id { get; set; }
+        public string titular { get; set; }
+        public string cpf_titular { get; set; }
+        public string numero { get; set; }
+        public string cvc { get; set; }
+        public DateTime vencimento { get; set; }
     }
     public class Endereco
     {
         //Cadastro endereço
-        public long Id { get; set; }
-        public string cep, estado, cidade, bairro, logradouro, numero, complemento;
+        public long id { get; set; }
+        public string cep { get; set; }
+        public string estado { get; set; }
+        public string cidade { get; set; }
+        public string bairro { get; set; }
+        public string logradouro { get; set; }
+        public string numero { get; set; }
+        public string complemento{ get; set; }
     }
 }

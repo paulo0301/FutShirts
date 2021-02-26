04cf6d43c0dac85aa5ede6ec6bdfcbbd2e7718ae
Paulo Daniel <pdanielcdo11@gmail.com>
2021-02-26 00:53:14 -0300

    tentando criar bd

--- aplicacao asp/FutShirt/Controllers/UsuarioController.cs
+++ aplicacao asp/FutShirt/Controllers/UsuarioController.cs
@@ -17,9 +17,8 @@ namespace FutShirt.Controllers
         // GET: Usuario
         public ActionResult Index()
         {
-            return View();
+            return View(usuarioContext.Usuarios.OrderBy(c => c.Nome));
         }
-
         // GET: Usuario/Create
         public ActionResult CreateStepOne()
         {
